59bf6a852a482aa3e6ec97a5f84b98478168081c
Yuri Numerov <achi@hush.ai>
2015-11-02 23:20:18 +0100

    another test attempt

--- tests/test.py
+++ tests/test.py
@@ -2,11 +2,17 @@ import unittest
 
 class TestSuite(unittest.TestCase):
 
-    def test(self):
-        self.failIf(1==0)
+  def test(self):
+    self.assertRaises(IndexError,[][1])
+
+  def test2(self):
+    self.assertEqual(range(10),range(10))
+
+  def test3(self):
+    self.assertEqual(1,1)     
 
 def main():
-    unittest.main()
+  unittest.main()
 
 if __name__ == "__main__":
-    main()
+  main()
