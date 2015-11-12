6911b6335fd76afce3c2183f7c58265e065f3c4e
Yuri Numerov <achi@hush.ai>
2015-11-12 16:12:35 +0100

    updated mob tests

--- tests/mob_test.py
+++ tests/mob_test.py
@@ -38,16 +38,16 @@ class Mobtestsuit(unittest.TestCase):
     for j in range(50):
       for i in range(10):
         tm=mob.mob(i)
-        self.assertTrue(tm.lock==0)
-        self.assertTrue(tm.hit==0)
-        self.assertTrue(tm.xpos==0)
-        self.assertTrue(tm.ypos==0)
-        self.assertTrue(len(tm.marker)==1)
+        self.assertEqual(tm.lock, 0)
+        self.assertEqual(tm.hit, 0)
+        self.assertEqual(tm.xpos, 0)
+        self.assertEqual(tm.ypos, 0)
+        self.assertEqual(len(tm.marker), 1)
         self.assertIsInstance(tm.marker, basestring)
         self.assertIsInstance(tm.level, int)
         self.assertIsInstance(tm.prestige, int)
         self.assertIsInstance(tm.exp, int)
-        self.assertTrue(tm.exp>=0)
+        self.assertGreaterEqual(tm.exp, 0)
         self.assertIsInstance(tm.INT, int)
         self.assertIsInstance(tm.DEX, int)
         self.assertIsInstance(tm.PER, int)
@@ -57,12 +57,12 @@ class Mobtestsuit(unittest.TestCase):
         self.assertIsInstance(tm.STR, int)
         self.assertIsInstance(tm.atk, int)
         self.assertIsInstance(tm.defn, int)
-        self.assertTrue(tm.atk>=0)
-        self.assertTrue(tm.defn>=0)
-        self.assertTrue(tm.MP>=0)
-        self.assertTrue(tm.HP>=0)
-        self.assertTrue(tm.END>=0)
-        self.assertTrue(tm.SPD>0)
+        self.assertGreaterEqual(tm.atk, 0)
+        self.assertGreaterEqual(tm.defn, 0)
+        self.assertGreaterEqual(tm.MP, 0)
+        self.assertGreaterEqual(tm.HP, 0)
+        self.assertGreaterEqual(tm.END, 0)
+        self.assertGreater(tm.SPD, 0)
 
 
   def test_mob_constructor_secondary(self):
@@ -77,10 +77,10 @@ class Mobtestsuit(unittest.TestCase):
         tm.secondary()
         for i in self.mobattributes: 
           self.assertIsInstance(eval("tm.%s"%i), int)
-        self.assertTrue(tm.HP==18)
-        self.assertTrue(tm.MP==2)
-        self.assertTrue(tm.END==14)
-        self.assertTrue(tm.SPD==6)
+        self.assertEqual(tm.HP, 18)
+        self.assertEqual(tm.MP, 2)
+        self.assertEqual(tm.END, 14)
+        self.assertEqual(tm.SPD, 6)
 
   def test_mob_movement(self):
     """
@@ -96,12 +96,12 @@ class Mobtestsuit(unittest.TestCase):
           tm.ypos=1
           ret=tm.move(td,i+1,1)
           if i+1==3: 
-            self.assertTrue(tm.ypos==2)
-            self.assertTrue(ret==0)
+            self.assertEqual(tm.ypos, 2)
+            self.assertEqual(ret, 0)
           else:
-            self.assertTrue(tm.ypos==1)
-            self.assertTrue(ret==1)
-          self.assertTrue(tm.xpos==1)
+            self.assertEqual(tm.ypos, 1)
+            self.assertEqual(ret, 1)
+          self.assertEqual(tm.xpos, 1)
 
   def test_mob_movement_random(self):
     """
@@ -114,7 +114,7 @@ class Mobtestsuit(unittest.TestCase):
     tm.ypos=1
     for i in range(100):
       tm.move(td,1)
-      self.assertTrue(tm.xpos==1)
+      self.assertEqual(tm.xpos, 1)
       self.assertIn(tm.ypos, [1,2])
 
   def test_mob_movement_trandom(self):
@@ -128,7 +128,7 @@ class Mobtestsuit(unittest.TestCase):
     tm.ypos=1
     for i in range(100):
       tm.move(td)
-      self.assertTrue(tm.xpos==1)
+      self.assertEqual(tm.xpos, 1)
       self.assertIn(tm.ypos, [1,2])
 
   def test_mob_search(self):
@@ -214,17 +214,17 @@ class Mobtestsuit(unittest.TestCase):
     tp.totalrcv=0
     tp.hp2=100
     out=tm.attack(tp)
-    self.assertTrue(tp.totalrcv==15)
+    self.assertEqual(tp.totalrcv, 15)
     self.assertIsInstance(tp.totalrcv, int)
-    self.assertTrue(tp.hp2==85)
+    self.assertEqual(tp.hp2, 85)
     self.assertIsInstance(tp.hp2, int)
     self.assertIn("##MOB##", out)
     self.assertIn("##PLAYER##", out)
     self.assertIsInstance(out, basestring)
-    self.assertTrue(tm.xpos==1)
-    self.assertTrue(tm.ypos==1)
-    self.assertTrue(tp.xpos==1)
-    self.assertTrue(tp.ypos==2)
+    self.assertEqual(tm.xpos, 1)
+    self.assertEqual(tm.ypos, 1)
+    self.assertEqual(tp.xpos, 1)
+    self.assertEqual(tp.ypos, 2)
     
 if __name__ == "__main__":
   unittest.main()
